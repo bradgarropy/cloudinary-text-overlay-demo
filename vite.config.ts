5b5c93cb96eaa9ed027341641197c0d821156804
Brad Garropy <bradgarropy@gmail.com>
2024-05-19 21:07:16 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
+import {defineConfig} from "vitest/config"
+
+const config = defineConfig({
+    plugins: [tsconfigPaths(), react()],
+    server: {
+        open: true,
+        port: 3000,
+    },
+    test: {
+        clearMocks: true,
+        coverage: {
+            all: false,
+            clean: true,
+            cleanOnRerun: true,
+            enabled: true,
+            provider: "v8",
+            reporter: ["text", "lcov"],
+            reportOnFailure: false,
+        },
+        environment: "jsdom",
+        globals: false,
+        passWithNoTests: true,
+        setupFiles: ["src/test-utils/setup.tsx"],
+    },
+})
+
+export default config
